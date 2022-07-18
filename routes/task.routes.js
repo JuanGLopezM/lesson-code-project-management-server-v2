bc653ab2eaf3f8fcc9e542bca750d4b3b7c20b5a
Juan G Lopez <nahujg@gmail.com>
2022-07-18 16:04:59 +0200

    no funciona

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -19,4 +19,53 @@ router.post("/tasks", (req, res, next) => {
     .catch((err) => res.json(err));
 });
 
+router.get("/tasks/:projectId", (req, res, next) => {
+  const { taskId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(taskId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  // Each Project document has `tasks` array holding `_id`s of Task documents
+  // We use .populate() method to get swap the `_id`s for the actual Task documents
+  Task.findById(taskId)
+    
+    .then((task) => res.status(200).json(task))
+    .catch((error) => res.json(error));
+});
+
+
+//  PUT /api/tasks  -  Edit a  task
+router.put("tasks/:taskId", (req, res, next) => {
+  const { taskId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(taskId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Task.findByIdAndUpdate(taskId, req.body, { new: true })
+    .then((updatedTask) => res.json(updatedTask))
+    .catch((error) => res.json(error));
+});
+
+// DELETE  /api/projects/:projectId  -  Deletes a specific project by id
+router.delete("tasks/:taskId", (req, res, next) => {
+  const { taskId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(taskId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Task.findByIdAndRemove(taskId)
+    .then(() =>
+      res.json({
+        message: `Task with ${taskId} is removed successfully.`,
+      })
+    )
+    .catch((error) => res.json(error));
+});
+
 module.exports = router;
